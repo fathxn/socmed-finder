f8bb7b0fd8d5b4be8319903a7069dc6e328c6d84
fathan0x1 <13360535+fathan0x1@users.noreply.github.com>
2019-06-13 21:59:10 +0700

    update socmed_finder.py
    update socmed_finder.py

--- socmed_finder.py
+++ socmed_finder.py
@@ -5,17 +5,30 @@
 import requests
 from colorama import Fore, Back, Style
 
-username = input("Input username: ")
+print(""" __                               _   ___ _           _           
+/ _\ ___   ___ _ __ ___   ___  __| | / __(_)_ __   __| | ___ _ __ 
+\ \ / _ \ / __| '_ ` _ \ / _ \/ _` |/ _\ | | '_ \ / _` |/ _ | '__|
+_\ | (_) | (__| | | | | |  __| (_| / /   | | | | | (_| |  __| |   
+\__/\___/ \___|_| |_| |_|\___|\__,_\/    |_|_| |_|\__,_|\___|_|
+                                         v1.1 - Author: @fathan0x1                                                              
+""")
 
-sites = ['https://www.facebook.com/',
-        'https://www.instagram.com/',
-        'https://wwww.ask.fm/',
-        'https://www.twitter.com/',
-        'https://www.pinterest.com/']
+username = input("[?] Input Username: ")
+print("[*] Finding",Style.BRIGHT+Fore.YELLOW+username+Style.RESET_ALL+"'s socmed ...\n")
 
-for i in sites:
-    r = requests.get(i+"{}".format(username))
-    if r.status_code == 200:
-        print(i+"{} --> ".format(username) + Fore.GREEN + "found!"+ Style.RESET_ALL)
-    else:
-        print(i+"{} --> ".format(username) + Fore.RED + "not found!"+ Style.RESET_ALL)
+def main(username):
+    sites = ['https://www.facebook.com/',
+            'https://www.instagram.com/',
+            'https://wwww.ask.fm/',
+            'https://www.twitter.com/',
+            'https://www.pinterest.com/',
+            'https://www.flickr.com/',]
+
+    for i in sites:
+        r = requests.get(i+"{}".format(username))
+        if r.status_code == 200:
+            print("[+] "+i+"{} --> ".format(username) + Fore.GREEN + "Found!"+ Style.RESET_ALL)
+        else:
+            print("[+] "+i+"{} --> ".format(username) + Fore.RED + "Not Found!"+ Style.RESET_ALL)
+
+main(username)
